2b069215f10cd90db15305611893ee61fd502ead
Jamie Dignam <jamie@101gamers.com>
2016-09-11 13:32:30 +0100

    echo and value testing
    Just for reference I'm going to upload my PHP files here too so that people can take a look at my code.

---
+++ echo.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>echoing shit</title>
+</head>
+<body>
+<?php
+echo "StapleButter is a dictator, a massive dictator. He should throw himself under a bus for not understanding that people can improve...";
+?>
+</body>
+</html>
---
+++ values.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Values and Variables</title>
+</head>
+<?php
+$var1 = 4718;
+$var2 = 471;
+$var3 = 443;
+
+echo($var1 + $var2 + $var3);
+?>
+</html>
